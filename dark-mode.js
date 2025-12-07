9d8a9b0c83acf0fb9ec0393994a19b93e520ff76
TheFormat <freddy1109@naver.com>
2025-12-07 18:26:47 +0900

    some fixes 12-7

--- dark-mode.js
+++ dark-mode.js
@@ -1,5 +1,12 @@
 document
   .querySelector("#dark-mode-toggle")
-  .addEventListener('click', () => {
+  .addEventListener('click', (event) => {
      document.body.classList.toggle("latex-dark");
-  });
+    const isDarkMode = document.body.classList.contains("latex-dark");
+    
+    if (isDarkMode) {
+        event.currentTarget.innerText = "밝게"; 
+    } else {
+        event.currentTarget.innerText = "어둡게";
+    }
+   });
